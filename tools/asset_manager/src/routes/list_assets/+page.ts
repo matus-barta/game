b283462e3f6a8fad597f856286e46058ea3bf67c
Matus Barta <matus.barta64@gmail.com>
2025-10-14 00:06:03 +0200

    try caching

--- tools/asset_manager/src/routes/list_assets/+page.ts
+++ tools/asset_manager/src/routes/list_assets/+page.ts
@@ -6,8 +6,12 @@ export const load: PageLoad = async ({ fetch }) => {
 		name: string;
 	}
 
-	const res = await fetch('http://localhost:3000/dev/model');
-	const models: model[] = await res.json();
+	try {
+		const res = await fetch('http://localhost:3000/dev/model');
 
-	return { models };
+		const models: model[] = await res.json();
+		return { models };
+	} catch (e) {
+		console.log(e);
+	}
 };
