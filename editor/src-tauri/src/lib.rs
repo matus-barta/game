881f18c4ae58e54003dfe3c5c582e8113258c36b
Matus Barta <matus.barta64@gmail.com>
2025-11-09 23:59:03 +0100

    editor : test custom titlebar

--- editor/src-tauri/src/lib.rs
+++ editor/src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use tauri::Manager;
+use tauri_plugin_decorum::WebviewWindowExt;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +11,17 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            // Create a custom titlebar for main window
+            // On Windows this will hide decoration and render custom window controls
+            // On macOS it expects a hiddenTitle: true and titleBarStyle: overlay
+            let main_window = app.get_webview_window("main").unwrap();
+            main_window.create_overlay_titlebar().unwrap();
+
+            #[cfg(target_os = "macos")]
+            //main_window.set_traffic_lights_inset(0.0, 20.0).unwrap();
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
