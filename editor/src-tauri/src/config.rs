973f1f8adae2145d45f15cc46c65d9d33a6f7e86
Matus Barta <matus.barta64@gmail.com>
2025-12-25 18:12:46 +0100

    may be deployable

--- editor/src-tauri/src/config.rs
+++ editor/src-tauri/src/config.rs
@@ -9,5 +9,8 @@ pub fn get_config() -> Result<Config, serde_json::Error> {
     #[cfg(debug_assertions)]
     let config = include_str!("../../local.config.json");
 
+    #[cfg(not(debug_assertions))]
+    let config = include_str!("../../config.json");
+
     serde_json::from_str::<Config>(config)
 }
